0ea1a739cdfce16c46592378b0861d0393c29d37
enockC <58557480+enockC@users.noreply.github.com>
2020-01-09 12:05:12 +0200

    Add project files.

---
+++ Operations/Addition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Operations
+{
+    public class Addition
+    {
+        public static int Sum(int a, int b) 
+        {
+            return a + b;
+        }
+    }
+}
---
+++ OperationsTests/AdditionTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations.Tests
+{
+    [TestClass()]
+    public class AdditionTests
+    {
+        private int a = 10;
+        private int b = 20;
+
+
+        [TestMethod()]
+        public void SumTest()
+        {
+            Assert.AreEqual(30, Addition.Sum(a, b));
+        }
+    }
+}
